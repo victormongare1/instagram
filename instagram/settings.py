e89ab3a837b54d6ff7c1d3c64a11bf3d58462bc9
victormongare1 <vicmongz254@gmail.com>
2020-05-29 21:19:40 +0300

    set up environment variables

--- instagram/settings.py
+++ instagram/settings.py
@@ -14,7 +14,7 @@ import os
 import os
 import django_heroku
 import dj_database_url
-from decouple import Csv,config
+from decouple import Csv,c
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
