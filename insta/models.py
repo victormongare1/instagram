ac0c3954c428b4ad12285fbd3a81cc852187e8c8
victormongare1 <vicmongz254@gmail.com>
2020-05-30 11:12:49 +0300

    created profile model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    '''
+    profile class to define profile objects
+    '''
+    profile_pic=models.ImageField(upload_to = 'images/')
+    bio=models.CharField(max_length = 100)
+
+class Image(models.Model):
+    '''
+    image class to define image objects
+    '''
+    image=models.ImageField(upload_to = 'images/')
+    name=
+    caption=
+    profile=
+    likes=
+    comments=
