7530d64ecde30180d7442d42803d4e41b4c1d51a
victormongare1 <vicmongz254@gmail.com>
2020-05-30 12:26:37 +0300

    created view for home page

--- insta/urls.py
+++ insta/urls.py
@@ -4,7 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-
+    url(r'^$',views.home,name='home'),
+    url(r'^search/',views.search_results,name="search_results"),
+    url(r'^newpost',views.new_post,name="new_post"),
+    url(r'^accounts/profile',views.profile,name='profile')
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
 if settings.DEBUG:
-  urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required.
+from .models import Image,Profile
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    posts=Image.objects.all()
+    return(request, 'Index.html',{"posts":posts})
+
+@login_required(login_url='/accounts/login/')  
+def search_results(request):
+    search=None
+
+@login_required(login_url='/accounts/login/')  
+def profile(request):
+    profile=None
+
+@login_required(login_url='/accounts/login/')  
+def new_post(request):
+    post=None
+
+
+
